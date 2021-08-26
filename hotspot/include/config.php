17d4fecf867f3e2fee45d61647c2913191784867
jaybeeumbay <jaybeeumbay@gmail.com>
2021-08-26 10:10:27 +0800

    add hotspot

---
+++ hotspot/include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['Jaabee'] = array ('1'=>'Jaabee!10.0.0.1','Jaabee@|@admin','Jaabee#|#r5aTo2Jkaw==','Jaabee%hsprof1','Jaabee^jaabee.hotspot','Jaabee&Php','Jaabee*10','Jaabee(1','Jaabee)','Jaabee=10','Jaabee@!@disable');
+$data['new-1783'] = array ('1'=>'new-1783!','new-1783@|@','new-1783#|#','new-1783%','new-1783^','new-1783&Rp','new-1783*10','new-1783(1','new-1783)','new-1783=10','new-1783@!@disable');
